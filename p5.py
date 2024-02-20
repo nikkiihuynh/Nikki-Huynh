bd961d9b5e101d5caaef5c2c82fa76cc3e53bd61
nikkiihuynh <98788799+nikkiihuynh@users.noreply.github.com>
2024-02-19 19:10:23 -0800

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,32 @@
+# p5.py
+# Nikki Huynh
+# 6/30/22
+# Python 3.8.1
+# Description: Program to compute the sum and product of two numbers entered by the user.
+
+#enter values:
+num1 = int(input("enter first number:"))
+num2 = int(input("enter second number:"))
+
+#calculate:
+sum = num1 + num2
+#calculate:
+product = num1 * num2
+
+#results:
+print(num1,'+',num2,'=',sum)
+print(num1,'*',num2,'=',product)
+print("The sum of", num1, "and", num2, "is", sum)
+print("The product of", num1, "and", num2, "is", product)
+
+'''
+>>>
+============ RESTART: C:/Users/nikki/Downloads/intro to python/p5.py ===========
+enter first number:12
+enter second number:5
+12 + 5 = 17
+12 * 5 = 60
+The sum of 12 and 5 is 17
+The product of 12 and 5 is 60
+>>>
+'''
