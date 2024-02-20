11780e6f74e050e734935f9767e3014195dd281b
nikkiihuynh <98788799+nikkiihuynh@users.noreply.github.com>
2024-02-19 19:07:09 -0800

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,40 @@
+# p4.py
+# Nikki Huynh
+# 6/30/22
+# Python 3.8.1
+# Description: Program to take character input from keyboard and outputs a large block letter "C"  composed of that character
+
+x = input("Enter a letter: ")
+
+print("************************************************")
+print()
+print("            %s %s %s"   %(x,x,x))
+print("          %s      %s"    %(x,      x))
+print("         %s"            %(x))
+print("        %s" %(x))
+print("        %s" %(x))
+print("        %s" %(x))
+print("         %s" %(x))
+print("          %s      %s"    %(x,      x))
+print("            %s %s %s"   %(x,x,x))
+
+print("************************************************")
+
+'''
+>>>
+============ RESTART: C:/Users/nikki/Downloads/intro to python/p4.py ===========
+Enter a letter: D
+************************************************
+
+            D D D
+          D      D
+         D
+        D
+        D
+        D
+         D
+          D      D
+            D D D
+************************************************
+>>>
+'''
